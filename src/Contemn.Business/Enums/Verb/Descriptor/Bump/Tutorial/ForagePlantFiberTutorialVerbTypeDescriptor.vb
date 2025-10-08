2c8c871b9092a92c1a363657d4f679e590d40a03
TheGrumpyGameDev <ernestpazera@gmail.com>
2025-10-08 08:37:14 -0500

    common base class

--- src/Contemn.Business/Enums/Verb/Descriptor/Bump/Tutorial/ForagePlantFiberTutorialVerbTypeDescriptor.vb
+++ src/Contemn.Business/Enums/Verb/Descriptor/Bump/Tutorial/ForagePlantFiberTutorialVerbTypeDescriptor.vb
@@ -1,24 +1,14 @@
 ﻿Imports TGGD.Business
 
 Friend Class ForagePlantFiberTutorialVerbTypeDescriptor
-    Inherits VerbTypeDescriptor
+    Inherits ForageTutorialVerbTypeDescriptor
 
     Public Sub New()
         MyBase.New(
             NameOf(ForagePlantFiberTutorialVerbTypeDescriptor),
-            Business.VerbCategoryType.Bump,
-            "Tutorial: Forage for Plant Fiber")
-    End Sub
-
-    Friend Overrides Function Perform(character As ICharacter) As IDialog
-        If character.HasItemsOfType(NameOf(PlantFiberItemTypeDescriptor)) Then
-            Return Success(character)
-        End If
-        Return Failure(character)
-    End Function
-
-    Private Function Failure(character As ICharacter) As IDialog
-        Return New OkDialog(
+            "Tutorial: Forage for Plant Fiber",
+            NameOf(PlantFiberItemTypeDescriptor),
+            TagType.CompletedForagePlantFiberTutorial,
             {
                 New DialogLine(MoodType.Info, "Forage for Plant Fiber!"),
                 New DialogLine(MoodType.Info, "1. Move onto Grass"),
@@ -27,38 +17,8 @@ Friend Class ForagePlantFiberTutorialVerbTypeDescriptor
                 New DialogLine(MoodType.Info, "4. Repeat until you get some Plant Fiber"),
                 New DialogLine(MoodType.Info, "5. Come back")
             },
-            BumpDialog.LaunchMenu(character))
-    End Function
-
-    Private Function Success(character As ICharacter) As IDialog
-        character.SetTag(TagType.CompletedForagePlantFiberTutorial, True)
-
-        Return New OkDialog(
             {
                 New DialogLine(MoodType.Info, "Plant Fiber is used to Craft things.")
-            }.Concat(RestoreStats(character)),
-            BumpDialog.LaunchMenu(character))
-    End Function
-
-    Private Function RestoreStats(character As ICharacter) As IEnumerable(Of DialogLine)
-        Return {
-                New DialogLine(MoodType.Info, "Reward:"),
-                Restore(character, StatisticType.Hydration),
-                Restore(character, StatisticType.Satiety)
-            }
-    End Function
-
-    Private Shared Function Restore(character As ICharacter, statisticType As String) As DialogLine
-        Dim delta = character.GetStatisticMaximum(statisticType) - character.GetStatistic(statisticType)
-        character.ChangeStatistic(statisticType, delta)
-        Return New DialogLine(MoodType.Info, $"+{delta} {character.FormatStatistic(statisticType)}")
-    End Function
-
-    Friend Overrides Function CanPerform(character As ICharacter) As Boolean
-        Dim bumpLocation = character.GetBumpLocation()
-        Return MyBase.CanPerform(character) AndAlso
-            bumpLocation IsNot Nothing AndAlso
-            bumpLocation.GetTag(TagType.IsTutorialHouse) AndAlso
-            Not character.GetTag(TagType.CompletedForagePlantFiberTutorial)
-    End Function
+            })
+    End Sub
 End Class
---
+++ src/Contemn.Business/Enums/Verb/Descriptor/Bump/Tutorial/ForageTutorialVerbTypeDescriptor.vb
@@ -0,0 +1,71 @@
+﻿Imports TGGD.Business
+
+Friend Class ForageTutorialVerbTypeDescriptor
+    Inherits VerbTypeDescriptor
+
+    Private ReadOnly itemType As String
+    Private ReadOnly tagType As String
+    Private ReadOnly failureLines As IEnumerable(Of IDialogLine)
+    Private ReadOnly successLines As IEnumerable(Of IDialogLine)
+
+    Public Sub New(
+                  verbType As String,
+                  verbTypeName As String,
+                  itemType As String,
+                  tagType As String,
+                  failureLines As IEnumerable(Of IDialogLine),
+                  successLines As IEnumerable(Of IDialogLine))
+        MyBase.New(
+            verbType,
+            Business.VerbCategoryType.Bump,
+            verbTypeName)
+        Me.itemType = itemType
+        Me.failureLines = failureLines
+        Me.successLines = successLines
+        Me.tagType = tagType
+    End Sub
+
+    Friend Overrides Function Perform(character As ICharacter) As IDialog
+        If character.HasItemsOfType(itemType) Then
+            Return Success(character)
+        End If
+        Return Failure(character)
+    End Function
+
+    Private Function Failure(character As ICharacter) As IDialog
+        Return New OkDialog(
+            failureLines,
+            BumpDialog.LaunchMenu(character))
+    End Function
+
+    Private Function Success(character As ICharacter) As IDialog
+        character.SetTag(tagType, True)
+
+        Return New OkDialog(
+            successLines.
+            Concat(RestoreStats(character)),
+            BumpDialog.LaunchMenu(character))
+    End Function
+
+    Private Shared Function RestoreStats(character As ICharacter) As IEnumerable(Of DialogLine)
+        Return {
+                New DialogLine(MoodType.Info, "Reward:"),
+                Restore(character, StatisticType.Hydration),
+                Restore(character, StatisticType.Satiety)
+            }
+    End Function
+
+    Private Shared Function Restore(character As ICharacter, statisticType As String) As DialogLine
+        Dim delta = character.GetStatisticMaximum(statisticType) - character.GetStatistic(statisticType)
+        character.ChangeStatistic(statisticType, delta)
+        Return New DialogLine(MoodType.Info, $"+{delta} {character.FormatStatistic(statisticType)}")
+    End Function
+
+    Friend Overrides Function CanPerform(character As ICharacter) As Boolean
+        Dim bumpLocation = character.GetBumpLocation()
+        Return MyBase.CanPerform(character) AndAlso
+            bumpLocation IsNot Nothing AndAlso
+            bumpLocation.GetTag(Business.TagType.IsTutorialHouse) AndAlso
+            Not character.GetTag(tagType)
+    End Function
+End Class
