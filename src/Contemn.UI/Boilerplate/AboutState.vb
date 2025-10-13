7fe3c2761ec89419baa105d02dc8bc40c6bc8ed3
TheGrumpyGameDev <ernestpazera@gmail.com>
2025-10-13 05:46:58 -0500

    minor tweak to about screen

--- src/Contemn.UI/Boilerplate/AboutState.vb
+++ src/Contemn.UI/Boilerplate/AboutState.vb
@@ -19,6 +19,8 @@ Friend Class AboutState
         Buffer.Write(0, y, "Jern Jam 2025 x SciAnts", Hue.LightGray, Hue.Black)
         y += 1
         Buffer.Write(0, y, "September 2025", Hue.LightGray, Hue.Black)
+        y += 1
+        Buffer.WriteCentered(Buffer.Rows \ 2 + 5, "Winner: Uniqueness Award", Hue.LightGreen, Hue.Black)
         y += 2
         Buffer.Write(0, y, "Main Inspirations:", Hue.LightGray, Hue.Black)
         y += 1
