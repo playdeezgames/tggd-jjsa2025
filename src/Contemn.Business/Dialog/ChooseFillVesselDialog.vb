b7cc5f730f17cd07d564b1df61654939c9299c60
TheGrumpyGameDev <ernestpazera@gmail.com>
2025-10-21 03:50:54 -0500

    i can fill things!

---
+++ src/Contemn.Business/Dialog/ChooseFillVesselDialog.vb
@@ -0,0 +1,71 @@
+﻿Imports System.Reflection.Metadata.Ecma335
+Imports TGGD.Business
+
+Friend Class ChooseFillVesselDialog
+    Inherits BaseDialog
+    Private ReadOnly character As ICharacter
+
+    Public Sub New(character As ICharacter)
+        MyBase.New(GenerateCaption(character), GenerateChoices(character), GenerateLines(character))
+        Me.character = character
+    End Sub
+
+    Private Shared Function GenerateLines(character As ICharacter) As IEnumerable(Of IDialogLine)
+        Return Array.Empty(Of IDialogLine)
+    End Function
+
+    Private Shared Function GenerateChoices(character As ICharacter) As IEnumerable(Of IDialogChoice)
+        Dim result As New List(Of IDialogChoice) From
+            {
+                New DialogChoice(NEVER_MIND_CHOICE, NEVER_MIND_TEXT)
+            }
+        result.AddRange(
+            character.Items.Where(AddressOf IsFillableItem).
+            Select(Function(x) New DialogChoice(x.ItemId.ToString(), x.Name)))
+        Return result
+    End Function
+
+    Private Shared Function GenerateCaption(character As ICharacter) As String
+        Return "Fill..."
+    End Function
+
+    Public Overrides Function Choose(choice As String) As IDialog
+        Select Case choice
+            Case NEVER_MIND_CHOICE
+                Return CancelDialog()
+            Case Else
+                Return FillVessel(CInt(choice))
+        End Select
+    End Function
+
+    Private Function FillVessel(itemId As Integer) As IDialog
+        Dim item = character.World.GetItem(itemId)
+        item.SetStatistic(StatisticType.Water, item.GetStatisticMaximum(StatisticType.Water))
+        item.SetTag(TagType.Safe, False)
+        Return New OkDialog(
+            "You filled it!",
+            character.World.ProcessTurn().
+            Append(New DialogLine(MoodType.Info, $"Filled {item.Name}")),
+            ChooseFillVesselDialog.LaunchMenu(character))
+    End Function
+
+    Public Overrides Function CancelDialog() As IDialog
+        Return BumpDialog.LaunchMenu(character).Invoke
+    End Function
+
+    Friend Shared Function LaunchMenu(character As ICharacter) As Func(Of IDialog)
+        Dim emptyDialog As IDialog = Nothing
+        Return If(
+            HasFillableItem(character),
+            Function() New ChooseFillVesselDialog(character),
+            BumpDialog.LaunchMenu(character))
+    End Function
+
+    Private Shared Function HasFillableItem(character As ICharacter) As Boolean
+        Return character.Items.Any(AddressOf IsFillableItem)
+    End Function
+
+    Private Shared Function IsFillableItem(item As IItem) As Boolean
+        Return item.GetTag(TagType.IsFillable) AndAlso Not item.IsStatisticAtMaximum(StatisticType.Water)
+    End Function
+End Class
--- src/Contemn.Business/Enums/Verb/Descriptor/Bump/Water/FillVerbTypeDescriptor.vb
+++ src/Contemn.Business/Enums/Verb/Descriptor/Bump/Water/FillVerbTypeDescriptor.vb
@@ -11,14 +11,7 @@ Friend Class FillVerbTypeDescriptor
     End Sub
 
     Friend Overrides Function Perform(character As ICharacter) As IDialog
-        Dim item = character.Items.First(AddressOf GetFillableItem)
-        item.SetStatistic(StatisticType.Water, item.GetStatisticMaximum(StatisticType.Water))
-        item.SetTag(TagType.Safe, False)
-        Return New OkDialog(
-            "You filled it!",
-            character.World.ProcessTurn().
-            Append(New DialogLine(MoodType.Info, $"Filled {item.Name}")),
-            CharacterActionsDialog.LaunchMenu(character))
+        Return ChooseFillVesselDialog.LaunchMenu(character).Invoke
     End Function
 
     Friend Overrides Function CanPerform(character As ICharacter) As Boolean
@@ -26,10 +19,10 @@ Friend Class FillVerbTypeDescriptor
         Return MyBase.CanPerform(character) AndAlso
             bumpLocation IsNot Nothing AndAlso
             bumpLocation.GetTag(TagType.CanFill) AndAlso
-            character.Items.Any(AddressOf GetFillableItem)
+            character.Items.Any(AddressOf IsFillableItem)
     End Function
 
-    Private Function GetFillableItem(item As IItem) As Boolean
+    Private Function IsFillableItem(item As IItem) As Boolean
         Return item.GetTag(TagType.IsFillable) AndAlso Not item.IsStatisticAtMaximum(StatisticType.Water)
     End Function
 End Class
