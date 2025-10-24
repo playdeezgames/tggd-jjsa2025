65382a3620af6a68509e24a703e42734b7d61e43
TheGrumpyGameDev <ernestpazera@gmail.com>
2025-10-24 04:00:58 -0500

    basedialog

--- src/Contemn.Business/Dialog/Base/BaseDialog.vb
+++ src/Contemn.Business/Dialog/Base/BaseDialog.vb
@@ -1,23 +1,26 @@
 ﻿Imports TGGD.Business
 
 Friend MustInherit Class BaseDialog
-    Inherits LegacyBaseDialog
+    Implements IDialog
 
     Private ReadOnly onCancelDialog As Func(Of IDialog)
+    Public ReadOnly Property Caption As String Implements IDialog.Caption
+    Public ReadOnly Property Choices As IEnumerable(Of IDialogChoice) Implements IDialog.Choices
+    Public ReadOnly Property Lines As IEnumerable(Of IDialogLine) Implements IDialog.Lines
+    Public MustOverride Function Choose(choice As String) As IDialog Implements IDialog.Choose
 
     Public Sub New(
                   caption As String,
                   choices As IEnumerable(Of IDialogChoice),
                   lines As IEnumerable(Of IDialogLine),
                   onCancelDialog As Func(Of IDialog))
-        MyBase.New(
-            caption,
-            choices,
-            lines)
+        Me.Caption = caption
+        Me.Choices = choices
+        Me.Lines = lines
         Me.onCancelDialog = onCancelDialog
     End Sub
 
-    Public Overrides Function CancelDialog() As IDialog
+    Public Function CancelDialog() As IDialog Implements IDialog.CancelDialog
         Return onCancelDialog()
     End Function
 End Class
