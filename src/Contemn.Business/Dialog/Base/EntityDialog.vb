40bdc785535561b74538f63a8819cc58e1da88cc
TheGrumpyGameDev <ernestpazera@gmail.com>
2025-10-24 05:19:25 -0500

    EntityDialog

---
+++ src/Contemn.Business/Dialog/Base/EntityDialog.vb
@@ -0,0 +1,21 @@
+﻿Imports TGGD.Business
+
+Friend MustInherit Class EntityDialog(Of TEntity As IEntity)
+    Inherits BaseDialog
+
+    Protected ReadOnly entity As TEntity
+
+    Sub New(
+           entity As TEntity,
+           generateCaption As Func(Of TEntity, String),
+           generateChoices As Func(Of TEntity, IEnumerable(Of IDialogChoice)),
+           generateLines As Func(Of TEntity, IEnumerable(Of IDialogLine)),
+           onCancelDialog As Func(Of IDialog))
+        MyBase.New(
+            generateCaption(entity),
+            generateChoices(entity),
+            generateLines(entity),
+            onCancelDialog)
+        Me.entity = entity
+    End Sub
+End Class
