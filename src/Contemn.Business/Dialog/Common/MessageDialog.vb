db9b0ee00cd56d3f2fddef6f272d7998d8e65ef8
TheGrumpyGameDev <ernestpazera@gmail.com>
2025-10-24 04:07:04 -0500

    MessageDialog

--- src/Contemn.Business/Dialog/Common/MessageDialog.vb
+++ src/Contemn.Business/Dialog/Common/MessageDialog.vb
@@ -1,26 +1,24 @@
 ﻿Imports TGGD.Business
 
 Friend Class MessageDialog
-    Inherits LegacyBaseDialog
+    Inherits BaseDialog
 
-    Private ReadOnly onCancel As Func(Of IDialog)
     Private ReadOnly choiceTable As IReadOnlyDictionary(Of String, Func(Of IDialog))
 
     Public Sub New(
                   caption As String,
                   lines As IEnumerable(Of IDialogLine),
                   choices As IEnumerable(Of (Choice As String, Text As String, NextDialog As Func(Of IDialog), Enabled As Boolean)),
-                  cancelDialog As Func(Of IDialog))
-        MyBase.New(caption, choices.Where(Function(x) x.Enabled).Select(Function(x) New DialogChoice(x.Choice, x.Text)), lines)
+                  onCancelDialog As Func(Of IDialog))
+        MyBase.New(
+            caption,
+            choices.Where(Function(x) x.Enabled).Select(Function(x) New DialogChoice(x.Choice, x.Text)),
+            lines,
+            onCancelDialog)
         Me.choiceTable = choices.Where(Function(x) x.Enabled).ToDictionary(Function(x) x.Choice, Function(x) x.NextDialog)
-        Me.onCancel = cancelDialog
     End Sub
 
     Public Overrides Function Choose(choice As String) As IDialog
         Return choiceTable(choice).Invoke()
-    End Function
-
-    Public Overrides Function CancelDialog() As IDialog
-        Return onCancel()
     End Function
 End Class
